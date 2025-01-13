873d69b5c8a0656f141a7cf5063f5873689a9c5c
NATAN VELASCO PEDROSO <natan.vpedroso@senacsp.edu.br>
2025-01-13 19:57:05 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Ex.02 - Escreva um programa que pergunte o nome do usuário, armazene esse nome numa variável name e apresente uma mensagem de boas-vindas exibindo o nome do usuário
+
+Console.WriteLine("Informe o seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a) " + name);
+
+ 
