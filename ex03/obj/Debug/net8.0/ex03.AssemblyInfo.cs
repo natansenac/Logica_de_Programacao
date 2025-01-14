bfb1b5afe36de099387bbac66adf6dbf6964e0cf
NATAN VELASCO PEDROSO <natan.vpedroso@senacsp.edu.br>
2025-01-13 22:05:04 -0300

    ex04

--- ex03/obj/Debug/net8.0/ex03.AssemblyInfo.cs
+++ ex03/obj/Debug/net8.0/ex03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873d69b5c8a0656f141a7cf5063f5873689a9c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ex04/Program.cs
@@ -0,0 +1,36 @@
+﻿// Tipos de dados 
+
+string nome = "Natan";
+Console.WriteLine("O nome do usuário é: " + nome);
+
+int idade = 18;
+//Console.WriteLine("A idade do usuário é " + idade + "anos");
+Console.WriteLine($"A idade do usuário é: {idade} anos ");
+
+float carteira = 2.63f;
+
+char classificacao = 'A';
+Console.WriteLine($"O cliente é do tipo {classificacao}");
+
+bool dinheiroNaConta = true;
+Console.WriteLine(dinheiroNaConta);
+
+double limiteCrédito =  2000.00;
+Console.WriteLine("Seu limite de crédito é: " + limiteCrédito);
+
+string sobrenome = " Velasco";
+
+string nomeCompleto = nome + sobrenome;
+Console.WriteLine(nomeCompleto);
+
+double valorConta = 53.73;
+Console.WriteLine($"O valor na conta é: {valorConta}");
+
+double limiteTotal = (valorConta + limiteCrédito);
+Console.WriteLine($"O saldo + Limite é`{limiteTotal}");
+
+
+string time = "Corinthians";
+Console.WriteLine("O time do usuário é: " + time);
+
+
